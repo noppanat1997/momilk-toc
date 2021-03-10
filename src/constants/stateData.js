dc9269087aff467197ec1266177e55e7e9b3d384
Thanakit Charoenboonvivat <54524621+m1crochip@users.noreply.github.com>
2021-03-11 00:32:01 +0700

    Commit create node and link without Trap state

--- src/constants/stateData.js
+++ src/constants/stateData.js
@@ -160,31 +160,66 @@ export const INITIAL_STATE_DATA = {
     { key: -8, from: 2, to: 6, color: "#fff281", zOrder: 1 },
     { key: -9, from: 2, to: 7, color: "#fff281", zOrder: 1 },
     { key: -10, from: 2, to: 8, color: "#fff281", zOrder: 1 },
+    //Loop Layer1
+    { key: -11, from: 1, to: 1, color: "#fff281", zOrder: 1 },
+    { key: -12, from: 2, to: 2, color: "#fff281", zOrder: 1 },
     //Layer 2 -> Layer 2
-    { key: -11, from: 3, to: 4, color: "#fff281", zOrder: 1 },
-    { key: -12, from: 3, to: 5, color: "#fff281", zOrder: 1 },
+    { key: -13, from: 3, to: 4, color: "#fff281", zOrder: 1 },
+    { key: -14, from: 3, to: 5, color: "#fff281", zOrder: 1 },
 
-    { key: -13, from: 4, to: 3, color: "#fff281", zOrder: 1 },
-    { key: -14, from: 4, to: 5, color: "#fff281", zOrder: 1 },
+    { key: -15, from: 4, to: 3, color: "#fff281", zOrder: 1 },
+    { key: -16, from: 4, to: 5, color: "#fff281", zOrder: 1 },
 
-    { key: -15, from: 5, to: 3, color: "#fff281", zOrder: 1 },
-    { key: -16, from: 5, to: 4, color: "#fff281", zOrder: 1 },
+    { key: -17, from: 5, to: 3, color: "#fff281", zOrder: 1 },
+    { key: -18, from: 5, to: 4, color: "#fff281", zOrder: 1 },
 
-    { key: -17, from: 6, to: 7, color: "#fff281", zOrder: 1 },
-    { key: -18, from: 6, to: 8, color: "#fff281", zOrder: 1 },
+    { key: -19, from: 6, to: 7, color: "#fff281", zOrder: 1 },
+    { key: -20, from: 6, to: 8, color: "#fff281", zOrder: 1 },
 
-    { key: -19, from: 7, to: 6, color: "#fff281", zOrder: 1 },
-    { key: -20, from: 7, to: 8, color: "#fff281", zOrder: 1 },
+    { key: -21, from: 7, to: 6, color: "#fff281", zOrder: 1 },
+    { key: -22, from: 7, to: 8, color: "#fff281", zOrder: 1 },
 
-    { key: -21, from: 8, to: 6, color: "#fff281", zOrder: 1 },
-    { key: -22, from: 8, to: 7, color: "#fff281", zOrder: 1 },
+    { key: -23, from: 8, to: 6, color: "#fff281", zOrder: 1 },
+    { key: -24, from: 8, to: 7, color: "#fff281", zOrder: 1 },
     //Layer 2 -> Layer 1
-    { key: -23, from: 3, to: 1, color: "#fff281", zOrder: 1 },
-    { key: -24, from: 4, to: 1, color: "#fff281", zOrder: 1 },
-    { key: -25, from: 5, to: 1, color: "#fff281", zOrder: 1 },
+    { key: -25, from: 3, to: 1, color: "#fff281", zOrder: 1 },
+    { key: -26, from: 4, to: 1, color: "#fff281", zOrder: 1 },
+    { key: -27, from: 5, to: 1, color: "#fff281", zOrder: 1 },
 
-    { key: -26, from: 6, to: 2, color: "#fff281", zOrder: 1 },
-    { key: -27, from: 7, to: 2, color: "#fff281", zOrder: 1 },
-    { key: -28, from: 8, to: 2, color: "#fff281", zOrder: 1 },
+    { key: -28, from: 6, to: 2, color: "#fff281", zOrder: 1 },
+    { key: -29, from: 7, to: 2, color: "#fff281", zOrder: 1 },
+    { key: -30, from: 8, to: 2, color: "#fff281", zOrder: 1 },
+    //Loop Layer 2
+    { key: -31, from: 3, to: 3, color: "#fff281", zOrder: 1 },
+    { key: -32, from: 4, to: 4, color: "#fff281", zOrder: 1 },
+    { key: -33, from: 5, to: 5, color: "#fff281", zOrder: 1 },
+
+    { key: -34, from: 6, to: 6, color: "#fff281", zOrder: 1 },
+    { key: -35, from: 7, to: 7, color: "#fff281", zOrder: 1 },
+    { key: -36, from: 8, to: 8, color: "#fff281", zOrder: 1 },
+    //Layer 2 -> Layer 3
+    { key: -37, from: 3, to: 9, color: "#fff281", zOrder: 1 },
+    { key: -38, from: 4, to: 10, color: "#fff281", zOrder: 1 },
+    { key: -39, from: 5, to: 11, color: "#fff281", zOrder: 1 },
+
+    { key: -40, from: 6, to: 12, color: "#fff281", zOrder: 1 },
+    { key: -41, from: 7, to: 13, color: "#fff281", zOrder: 1 },
+    { key: -42, from: 8, to: 14, color: "#fff281", zOrder: 1 },
+    //Layer 3 -> Layer 2
+    { key: -43, from: 9, to: 3, color: "#fff281", zOrder: 1 },
+    { key: -44, from: 10, to: 4, color: "#fff281", zOrder: 1 },
+    { key: -45, from: 11, to: 5, color: "#fff281", zOrder: 1 },
+
+    { key: -46, from: 12, to: 6, color: "#fff281", zOrder: 1 },
+    { key: -47, from: 13, to: 7, color: "#fff281", zOrder: 1 },
+    { key: -48, from: 14, to: 8, color: "#fff281", zOrder: 1 },
+    //Layer 2 -> Layer 4
+    { key: -49, from: 3, to: 15, color: "#fff281", zOrder: 1 },
+    { key: -50, from: 4, to: 15, color: "#fff281", zOrder: 1 },
+    { key: -51, from: 5, to: 15, color: "#fff281", zOrder: 1 },
+
+    { key: -52, from: 6, to: 15, color: "#fff281", zOrder: 1 },
+    { key: -53, from: 7, to: 15, color: "#fff281", zOrder: 1 },
+    { key: -54, from: 8, to: 15, color: "#fff281", zOrder: 1 },
   ],
 }
